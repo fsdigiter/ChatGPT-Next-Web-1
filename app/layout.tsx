51a5177abe446bdd85241ba5701053510ab29738
fsdigiter <129246001+fsdigiter@users.noreply.github.com>
2023-05-30 02:57:26 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 export const metadata = {
-  title: "ChatGPT Next Web",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "人工智能聊天机器人",
+  description: "人工智能聊天机器人",
   viewport: {
     width: "device-width",
     initialScale: 1,
@@ -19,7 +19,7 @@ export const metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "人工智能聊天机器人",
     statusBarStyle: "default",
   },
 };
